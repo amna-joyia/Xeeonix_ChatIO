7562c821efeaa6d3008d8afa76cd6b37692950ad
Amna Joyia <amna.joyia@xeeonix.com>
2015-08-04 13:41:06 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var express=require('express'),
+app=express(),
+server = require('http').createServer(app),
+io=require('socket.io').listen(server);
+usernames=[]; //Array of connected users
+
+server.listen(process.env.PORT || 3000);
+
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+	socket.on('new user' ,function(data,callback){
+		if(usernames.indexOf(data) != -1){
+			callback(false);
+		} else{
+			callback(true);
+			socket.username=data;
+			usernames.push(socket.username);
+			updateUsernames();
+		}
+	});
+	//update Usernames
+	function updateUsernames(){
+		io.sockets.emit('usernames',usernames); 
+	}
+	//send message
+	socket.on('send message', function(data){
+		     io.sockets.emit('new message',{msg:data, user:socket.username}); 
+		console.log(data);
+		});
+		//Disconnect
+		socket.on('disconnect',function(data){
+		 if(!socket.username) return;
+		 usernames.splice(usernames.indexOf(socket.username), 1); // a funstion to remove a sepcific function from array
+		 updateUsernames();
+		});
+});
+
+
+
+
